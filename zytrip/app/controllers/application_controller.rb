83cb3f9e56a6103042898dd47a4be958ebf8ed5e
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-03-23 13:28:17 +0100

    sidebar admin y arreglo rutas login

--- zytrip/app/controllers/application_controller.rb
+++ zytrip/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-	
+	protected
+
+	def after_sign_in_path_for(resource)
+		if current_user.admin == true
+			admin_dashboards_landing_path
+		else
+			trips_path
+		end
+	end
+
 end

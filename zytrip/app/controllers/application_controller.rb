c82c3e0d24c1de4d121d6e5bdfd8fe027cf76820
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-03-26 20:43:34 +0100

    Vista usuarios interfaz admin

--- zytrip/app/controllers/application_controller.rb
+++ zytrip/app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
 	protected
 
-	def after_sign_in_path_for(resource)
-		if current_user.admin == true
-			admin_dashboards_landing_path
-		else
-			trips_path
+		#redirect to admin_path if admin
+		def after_sign_in_path_for(resource)
+			if current_user.admin == true
+				admin_dashboards_landing_path
+			else
+				trips_path
+			end
 		end
-	end
+
+		#Add fields to devise register form
+		def configure_permitted_parameters
+	       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
+
+	       devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
+		end
 
 end

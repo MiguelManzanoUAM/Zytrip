1e77860824412c86e23f8bb25f4f287a0b66a037
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-03-27 21:25:03 +0200

    busqueda y add en panel admin de usuarios

--- zytrip/app/controllers/admin/users_controller.rb
+++ zytrip/app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@users = User.all
+		@users = User.search(params[:search])
 	end
 
 	def new
--- zytrip/app/models/user.rb
+++ zytrip/app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #Busqueda de usuarios mediante barra de busqueda
+  def self.search(search)
+    if search
+      where(["name like ?", "%#{search}%"])
+    else
+      all
+    end
+  end
 end

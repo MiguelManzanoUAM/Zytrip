2a03236492946cbcab7565327f3f1f10df83ce65
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-07-14 13:10:41 +0200

    first price filters

--- zytrip/app/controllers/trips_controller.rb
+++ zytrip/app/controllers/trips_controller.rb
@@ -3,6 +3,12 @@ class TripsController < ApplicationController
   # GET /trips or /trips.json
   def index
     @trips = Trip.all
+
+    if params[:minp].present? || params[:maxp].present?
+      @trips = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+    else
+      @trips = Trip.all
+    end 
   end
 
   # GET /trips/1 or /trips/1.json
@@ -10,5 +16,6 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
     @agency = Agency.find(@trip.agency_id)
   end
+
 
 end

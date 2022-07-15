f91eb89331cc158a8ede01e722e533855e31972e
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-07-15 13:49:36 +0200

    Filters with checkboxfirst version

--- zytrip/app/controllers/trips_controller.rb
+++ zytrip/app/controllers/trips_controller.rb
@@ -4,11 +4,21 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
 
-    if params[:minp].present? || params[:maxp].present?
-      @trips = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+    #if params[:minp].present? || params[:maxp].present?
+      #@trips = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+    #else
+      #@trips = Trip.all
+    #end
+
+    if params[:stars4]
+      @trips = Trip.where("price >= 300")
+    elsif params[:stars3]
+      @trips = Trip.where("price >= 900")
     else
       @trips = Trip.all
-    end 
+    end
+
+    
   end
 
   # GET /trips/1 or /trips/1.json

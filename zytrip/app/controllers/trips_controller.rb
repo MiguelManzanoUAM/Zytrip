55f16b40b67ba3555845e168285b9ab3705a7208
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-08-04 13:04:29 +0200

    sidebar new styles and improved filters

--- zytrip/app/controllers/trips_controller.rb
+++ zytrip/app/controllers/trips_controller.rb
@@ -4,21 +4,33 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
 
-    #if params[:minp].present? || params[:maxp].present?
-      #@trips = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
-    #else
-      #@trips = Trip.all
-    #end
-
-    if params[:stars4]
-      @trips = Trip.where("price >= 300")
-    elsif params[:stars3]
-      @trips = Trip.where("price >= 900")
+    if params[:minp].present? || params[:maxp].present?
+      @trips_price = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+      if params[:stars4]
+        @trips = @trips_price.where("rating >= 4")
+      elsif params[:stars3]
+        @trips = @trips_price.where("rating >= 3")
+      elsif params[:stars2]
+        @trips = @trips_price.where("rating >= 2")
+      elsif params[:stars1]
+        @trips = @trips_price.where("rating >= 1")
+      else
+        @trips = @trips_price
+      end
     else
-      @trips = Trip.all
+      if params[:stars4]
+        @trips = Trip.where("rating >= 4")
+      elsif params[:stars3]
+        @trips = Trip.where("rating >= 3")
+      elsif params[:stars2]
+        @trips = Trip.where("rating >= 2")
+      elsif params[:stars1]
+        @trips = Trip.where("rating >= 1")
+      else
+        @trips = Trip.all
+      end
     end
 
-    
   end
 
   # GET /trips/1 or /trips/1.json

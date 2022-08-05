f639c99cc1037735acdae29069cc85feb6765c3c
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-08-05 13:46:19 +0200

    SearchBar now works

--- zytrip/app/controllers/trips_controller.rb
+++ zytrip/app/controllers/trips_controller.rb
@@ -2,32 +2,67 @@ class TripsController < ApplicationController
 
   # GET /trips or /trips.json
   def index
+
     @trips = Trip.all
 
-    if params[:minp].present? || params[:maxp].present?
-      @trips_price = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
-      if params[:stars4]
-        @trips = @trips_price.where("rating >= 4")
-      elsif params[:stars3]
-        @trips = @trips_price.where("rating >= 3")
-      elsif params[:stars2]
-        @trips = @trips_price.where("rating >= 2")
-      elsif params[:stars1]
-        @trips = @trips_price.where("rating >= 1")
-      else
-        @trips = @trips_price
-      end
+    #########################################
+    # 1- busqueda por nombre, ciudad o pais
+    # 2- busqueda por filtros
+    #########################################
+
+    if params[:sch].present?
+      @trips = Trip.where("title like ? OR city like ? OR country like ?", params[:sch], params[:sch], params[:sch])
     else
-      if params[:stars4]
-        @trips = Trip.where("rating >= 4")
-      elsif params[:stars3]
-        @trips = Trip.where("rating >= 3")
-      elsif params[:stars2]
-        @trips = Trip.where("rating >= 2")
-      elsif params[:stars1]
-        @trips = Trip.where("rating >= 1")
+      if params[:minp].present?
+        if params[:maxp].present?
+          @trips_price = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+        else
+          @trips_price = Trip.where("price >= ?", params[:minp])
+        end
+
+        if params[:stars4]
+          @trips = @trips_price.where("rating >= 4")
+        elsif params[:stars3]
+          @trips = @trips_price.where("rating >= 3")
+        elsif params[:stars2]
+          @trips = @trips_price.where("rating >= 2")
+        elsif params[:stars1]
+          @trips = @trips_price.where("rating >= 1")
+        else
+          @trips = @trips_price
+        end
+
+      elsif params[:maxp].present?
+        if params[:minp].present?
+          @trips_price = Trip.where("price >= ? AND price <= ?", params[:minp], params[:maxp])
+        else
+          @trips_price = Trip.where("price <= ?", params[:maxp])
+        end
+
+        if params[:stars4]
+          @trips = @trips_price.where("rating >= 4")
+        elsif params[:stars3]
+          @trips = @trips_price.where("rating >= 3")
+        elsif params[:stars2]
+          @trips = @trips_price.where("rating >= 2")
+        elsif params[:stars1]
+          @trips = @trips_price.where("rating >= 1")
+        else
+          @trips = @trips_price
+        end
+
       else
-        @trips = Trip.all
+        if params[:stars4]
+          @trips = Trip.where("rating >= 4")
+        elsif params[:stars3]
+          @trips = Trip.where("rating >= 3")
+        elsif params[:stars2]
+          @trips = Trip.where("rating >= 2")
+        elsif params[:stars1]
+          @trips = Trip.where("rating >= 1")
+        else
+          @trips = Trip.all
+        end
       end
     end
 
@@ -38,6 +73,5 @@ class TripsController < ApplicationController
     @trip = Trip.find(params[:id])
     @agency = Agency.find(@trip.agency_id)
   end
-
 
 end

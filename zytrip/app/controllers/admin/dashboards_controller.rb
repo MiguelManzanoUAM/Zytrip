6426fa5538329ac0333f807b57a813e5f25056ac
MiguelManzanoUAM <miguel.manzanom@estudiante.uam.es>
2022-10-11 13:46:09 +0200

    insight recommendation algorithm with testing

--- zytrip/app/controllers/admin/dashboards_controller.rb
+++ zytrip/app/controllers/admin/dashboards_controller.rb
@@ -15,6 +15,11 @@ class Admin::DashboardsController < ApplicationController
 		if @user
 			@trips_rating = User.user_trips_rating(@user)
 			@preferences = User.user_trips_preferences(@user)
+			@preferences_values = User.user_calculate_preferences(@user)
+			@final_preferences = User.valuable_preferences(@user)
+			@candidates = User.user_candidate_preferences_matrix(@user)
+			@candidates_affinities = User.user_candidate_preferences_afinity(@user)
+			@final_candidates = User.most_afinity_preferences_trips(@user)
 		end
 	end
 end
--- zytrip/app/models/user.rb
+++ zytrip/app/models/user.rb
@@ -124,7 +124,10 @@ class User < ApplicationRecord
     return trips_with_rating
   end
 
-
+  #####################################################
+  # Obtenemos todas las preferencias de un usuario
+  # durante sus viajes
+  #####################################################
   def self.user_trips_preferences(user)
     user_reviews = Review.where(user_id: user.id)
     trips = []
@@ -150,7 +153,145 @@ class User < ApplicationRecord
   #####################################################
   # Algoritmo de recomendación por conocimiento
   # ---------------------------------------------------
-  # 1) obtenemos los viajes que ha realizado un usuario
-  #    junto a sus valoraciones
+  # 1) obtenemos todas las preferencias del usuario
+  # 2) Creamos un hash {preferencia, importancia}
+  # 3) Añadimos las preferencias como claves al hash
+  # 4) Para cada viaje del usuario comprobamos si 
+  #    tiene una preferencia y de ser así le sumamos a
+  #    dicha preferencia su valoración
+  # 5) Calculamos el total de los valores de las
+  #    preferencias
+  # 6) Asignamos a cada preferencia su importancia
+  #    importancia = valor actual / total valores prefs
+  #
   #####################################################
+  def self.user_calculate_preferences(user)
+    preferences = User.user_trips_preferences(user)
+    trips = user.trips
+
+    preferences_values = {}
+    total_pref_rating = 0
+
+    preferences.each do |pref|
+      preferences_values[pref] = 0
+    end
+
+    trips.each do |trip|
+      preferences.each do |pref|
+        if Trip.trip_has_preference(trip, pref)
+          preferences_values[pref] += Review.get_review(user, trip)
+        end
+      end
+    end
+
+    preferences_values.keys.each do |key|
+      total_pref_rating += preferences_values[key]
+    end
+
+    preferences_values.keys.each do |key|
+      preferences_values[key] = (preferences_values[key].to_f / total_pref_rating.to_f).round(3)
+    end
+
+    return preferences_values
+
+  end
+
+  #####################################################
+  # Obtenemos las preferencias más relevantes de
+  # un usuario
+  #####################################################
+  def self.valuable_preferences(user)
+    preferences = User.user_calculate_preferences(user)
+    final_preferences = []
+
+    preferences_sorted = preferences.sort {|a1,a2| a2[1]<=>a1[1]}.to_h
+
+    final_preferences = preferences_sorted.keys.first(3)
+
+    return final_preferences
+  end
+
+  #####################################################
+  # Obtenemos los viajes no realizados por el usuario
+  #####################################################
+  def self.not_visited_trips(user)
+    trips = []
+
+    Trip.all.each do |trip|
+      if !(user.trips.include? trip)
+        trips << trip
+      end
+    end
+
+    return trips
+  end
+
+  #####################################################
+  # Obtenemos la matriz de los candidatos de un usuario
+  # ---------------------------------------------------
+  # 1) Obtenemos los viajes que el usuario no ha hecho
+  # 2) Construimos hash {viaje, {pref, valoracion}}
+  #####################################################
+  def self.user_candidate_preferences_matrix(user)
+    trips = User.not_visited_trips(user)
+    preferences = User.user_calculate_preferences(user)
+
+    candidates = {}
+
+    trips.each do |trip|
+      candidates[trip.id] = {}
+
+      preferences.each do |pref, value|
+        if Trip.trip_has_preference(trip, pref)
+          candidates[trip.id][pref] = value
+        else
+          candidates[trip.id][pref] = 0
+        end
+      end
+    end
+
+    return candidates
+
+  end
+
+  #####################################################
+  # Obtenemos los viajes candidatos de un usuario
+  # ---------------------------------------------------
+  # El resultado será un hash {viaje, afinidad}
+  # donde la afinidad será el resultado de sumar la
+  # importancia de las preferencias que tiene el viaje
+  #####################################################
+  def self.user_candidate_preferences_afinity(user)
+    candidates = User.user_candidate_preferences_matrix(user)
+    afinities = {}
+
+    candidates.keys.each do |trip|
+      afinities[trip] = 0
+
+      candidates[trip].keys.each do |pref|
+        afinities[trip] += candidates[trip][pref]
+      end
+    end
+
+    return afinities
+  end
+
+  #####################################################
+  # Obtenemos los 3 viajes que más afinidad tengan con
+  # el usuario siguiendo la técnica de recomendación
+  # por conocimiento
+  #####################################################
+  def self.most_afinity_preferences_trips(user)
+    candidates_affinity = User.user_candidate_preferences_afinity(user)
+    candidates_sorted = candidates_affinity.sort {|a1,a2| a2[1]<=>a1[1]}.to_h
+
+    candidates_ids = candidates_sorted.keys.first(3)
+    candidates = []
+
+    candidates_ids.each do |trip_id|
+      candidates << Trip.find_by(id: trip_id)
+    end
+
+    return candidates
+  end
 end
